72698633a8c11f99099427701da1e0920c0c2895
danil0711 <70793172+danil0711@users.noreply.github.com>
2023-11-17 15:01:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector(".wrapper")
+    form = wrapper.querySelector(".form")
+    input = wrapper.querySelector(".form input")
+    btn = wrapper.querySelector(".form button")
+    img = wrapper.querySelector(".qr-code img")
+let currentValueInput;
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+
+    const inputValue = input.value.trim()
+    if (!inputValue || inputValue === currentValueInput) return;
+    currentValueInput = inputValue;
+
+    btn.textContent = 'Идёт создание QR-кода...'
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=201x200&data=${inputValue}`
+
+    img.addEventListener('load', ()=>{
+        wrapper.classList.add('active');
+        btn.textContent = 'Сгенерировать QR-код'
+    })
+
+    img.addEventListener('error', () => {
+        alert('Ошибка при загрузке изображения QR-кода. Попробуйте снова')
+        location.reload()
+    })
+
+    input.addEventListener('input', function(){
+        if(!this.value.trim() && wrapper.classList.contains('active')){
+            wrapper.classList.remove('active')
+        }
+    })
+    
+})
